3a4b34766fb4263a982e759cc5cd96807259dc03
Giles Boutel <fumblemouse@gmail.com>
2021-01-17 10:11:04 +1300

    update datepickr

--- td2/baseapp/static/baseapp/js/flatpickr.js
+++ td2/baseapp/static/baseapp/js/flatpickr.js
@@ -1,2 +1,17 @@
-flatpickr('#id_start_date', {enableTime:true, defaultDate: 'today', minDate: 'today'});
-flatpickr('#id_expiry_date', {enableTime:true, defaultDate: new Date().fp_incr(7), minDate: 'today'});
+
+let nowIsTheHour = new Date();
+
+function addDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+  }
+
+flatpickr('#id_start_date', {enableTime:true, 
+    defaultDate: nowIsTheHour, 
+    minDate: nowIsTheHour
+    });
+flatpickr('#id_expiry_date', {enableTime:true, 
+    defaultDate: addDays(nowIsTheHour, 7), 
+    minDate: nowIsTheHour
+    });
